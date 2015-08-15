8778d33cd3deebc594d8a0ef893cbbb4b86895b8
Dyno Hongjun Fu <dyno.fu@gmail.com>
2015-08-15 08:45:01 -0700

    code format

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 cd $(dirname $0)
 BASEDIR=$PWD
 cd - &>/dev/null
@@ -7,13 +8,16 @@ set -x
 
 [ -d ~/env.d ] && rm -f ~/env.d
 ln -sf ${BASEDIR}/env.d ~/env.d
+
 if [ $(uname) == "Linux" ]; then
-  ln -sf ${BASEDIR}/bashrc ~/.bashrc
-  if [ $TERM == "linux" ]; then
-    ln -sf ${BASEDIR}/keymap ~/.keymap
-  fi
+    ln -sf ${BASEDIR}/bashrc ~/.bashrc
+    if [ $TERM == "linux" ]; then
+	ln -sf ${BASEDIR}/keymap ~/.keymap
+    fi
+
 elif [ "$(uname)" == "Darwin" ]; then
   ln -sf ${BASEDIR}/bashrc ~/.bash_profile
+
 else
   ln -sf ${BASEDIR}/bashrc ~/.bash_profile
 fi
